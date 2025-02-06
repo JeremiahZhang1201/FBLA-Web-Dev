b277e9148627a09a75013e055a2544a1bee693f3
Your Name <your-email@example.com>
2025-02-06 15:49:29 -0500

    .

--- suss/src/config.js
+++ suss/src/config.js
@@ -1,4 +1,4 @@
 export const GOOGLE_MAPS_API_KEY = process.env.REACT_APP_GOOGLE_MAPS_API_KEY;
-export const ADMIN_EMAILS = ['shamsizafir@gmail.com.com']; // Change this list to your admin emails
+export const ADMIN_EMAILS = ['shamsizafir@gmail.com']; // Change this list to your admin emails
 export const AUTH0_DOMAIN = process.env.REACT_APP_AUTH0_DOMAIN;
 export const AUTH0_CLIENT_ID = process.env.REACT_APP_AUTH0_CLIENT_ID;
